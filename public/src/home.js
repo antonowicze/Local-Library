b76112c0b84dce1338d0bc88039de34fce023373
Emilia <90724866+antonowicze@users.noreply.github.com>
2021-11-16 20:59:26 -0500

    Update home.js

--- public/src/home.js
+++ public/src/home.js
@@ -1,58 +1,73 @@
 function getTotalBooksCount(books) {
-  return books.length;
+  return books.length
 }
 
 function getTotalAccountsCount(accounts) {
-  return accounts.length;
+  return accounts.length
 }
 
 function getBooksBorrowedCount(books) {
- let booksCheckedOut = books.filter(
-  (book) =>
-   book.borrows.filter((record) => record.returned === false).length);
- return booksCheckedOut.length;
+  let booksCheckedOut = books.filter(
+    (book) => book.borrows.filter((record) => record.returned === false).length,
+  )
+  return booksCheckedOut.length
+}
+
+
+function commonGenresHelper(accum, book) {
+  // get the genre of current book
+  let genre = book.genre
+  // get the object in accum that has "name === genre"
+  let genreInfo = accum.find((element) => element.name === genre)
+  // if an object was not found, create a new one and push it into accum
+  if (!genreInfo) {
+    const newGenreInfo = {
+      name: genre,
+      count: 1,
+    }
+    accum.push(newGenreInfo)
+  } else {
+    // if object was found, then add 1 to count
+    genreInfo.count++
+  }
+  return accum
 }
 
 function getMostCommonGenres(books) {
- let map = {};
- books.forEach((num) => {
-  if (map[num.genre]) {
-   map[num.genre]++;
-  } else {
-   map[num.genre] = 1;} });
- return Object.entries(map)
-  .map(([name, count]) => {
-   return {
-    name,
-    count}; 
- })
-  .sort((a, b) => b.count - a.count).slice(0, 5);
+  // create new array of most common genres
+  let result = books.reduce(commonGenresHelper, [])
+  // sort the array by count from greatest to least
+  result.sort((a, b) => b.count - a.count)
+  return result.slice(0, 5)
 }
 
+function getPopularBooksHelper(book) {
+  return { name: book.title, count: book.borrows.length }
+}
 
 function getMostPopularBooks(books) {
-  return books.map((book) => {
-    return { name: book.title, count: book.borrows.length};
-  }).sort((a, b) => (a.count < b.count ? 1 : -1)).slice(0, 5);
+  return books
+    .map(getPopularBooksHelper)
+    .sort((a, b) => (a.count < b.count ? 1 : -1))
+    .slice(0, 5)
 }
 
-
 function getMostPopularAuthors(books, authors) {
-  let result = [];
+  let result = []
   authors.forEach((author) => {
-   let theAuthor = {
-    name: `${author.name.first} ${author.name.last}`,
-     count: 0
-   };
-   books.forEach((book) => {
-    if (book.authorId === author.id) {    
-        theAuthor.count += book.borrows.length;
-     }
-   });
-   result.push(theAuthor);
-  });
-  return result.sort((a, b) => b.count - a.count).slice(0, 5);
- }
+    let theAuthor = {
+      name: `${author.name.first} ${author.name.last}`,
+      count: 0,
+    }
+    books.forEach((book) => {
+      if (book.authorId === author.id) {
+        theAuthor.count += book.borrows.length
+      }
+    })
+    result.push(theAuthor)
+  })
+  return result.sort((a, b) => b.count - a.count).slice(0, 5)
+}
 
 module.exports = {
   getTotalBooksCount,
@@ -61,4 +76,4 @@ module.exports = {
   getMostCommonGenres,
   getMostPopularBooks,
   getMostPopularAuthors,
-};
+}
